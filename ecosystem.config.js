d301117b55c335b891faf13437be6a79567fd567
chendapeng <pengpeng.chen@ximalaya.com>
2024-04-19 00:43:00 +0800

    feat: add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,13 @@ module.exports = {
     {
       name: PR,
       script: './dist/main.js',
+      env: {
+        DB_HOST: '47.101.50.136',
+        DB_PORT: '3306',
+        DB_USER: 'root',
+        DB_PWD: '291003932Cpp@',
+        DB_DATABASE: 'cost',
+      },
       instances: 2,
       exec_mode: 'cluster',
       max_memory_restart: '2G',
