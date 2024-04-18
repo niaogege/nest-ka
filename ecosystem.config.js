26a52ab9f55cf795c54becc2b34221ddbd18d325
chendapeng <pengpeng.chen@ximalaya.com>
2024-04-18 23:29:34 +0800

    feat: add change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
+const PR = 'nest-ka';
 module.exports = {
   apps: [
     {
-      name: 'nest-ka',
+      name: PR,
       script: './dist/main.js',
       instances: 2,
       exec_mode: 'cluster',
       max_memory_restart: '2G',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss:SS',
+      error_file: `/www/web/${PR}/error.log`,
+      out_file: `/www/web/${PR}/out.log`,
     },
   ],
 };
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log(process.env.DB_HOST, 'process.env.DB_HOST');
-    return 'Hello World!' + process.env.DB_HOST;
+    return 'Hello World!' + process.env.DB_HOST + process.env.DB_USER;
   }
 }
